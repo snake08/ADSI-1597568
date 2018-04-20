c3f71302fdecf1296b87c0f55a8d5e438288e6ff
Alex <Alex@10.72.133.55>
2018-04-20 12:19:01 -0500

    This is miriada X,psudocodigo,cv

---
+++ Analisis/Myfirstproject/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'manners.php';
+
+use Myfirstproject\blink;
+
+$blink = new blink ( ) ;
+$blink->name();
---
+++ Analisis/Myfirstproject/manners.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Myfirstproject;
+
+class blink {
+
+  public function name () {
+    echo 'Welcome, people';
+  }
+
+}
